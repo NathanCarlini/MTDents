3efa0617ec82994eab7be84f9888ed5d9e5c7a1e
GhostProgrammer666 <nathan1.carlini@gmail.com>
2023-03-07 12:03:46 +0100

    ajout de entetequiz

---
+++ src/components/EnTeteQuiz.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function EnTeteQuiz() {
+  return (
+    <div className='flex flex-col'>
+    <p className="text-lg font-light text-textBlue"> nomDuQuizz</p>
+
+    <p className="text-lg font-semibold text-textBlue"> Question 1</p>
+    </div>
+  )
+}
